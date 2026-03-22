b8fcfab6a772ab85c0fb5b1bfe79ad613b6c67a5
unknown <vs2299043@gmail.com>
2026-03-22 20:06:27 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'remixicon/fonts/remixicon.css'
+import './index.css'
+const App = ()=>{
+  return (
+      <>
+        <h1 className='bg-blue-500'>hello</h1>
+      </>
+    )
+}
+export default App 
